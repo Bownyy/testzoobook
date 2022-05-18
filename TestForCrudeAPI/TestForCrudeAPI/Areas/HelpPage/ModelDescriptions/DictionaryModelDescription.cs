caeaa5af8fe5726c301a28a5cba67125f506610c
anthonytejo <john.tejo@transcom.com>
2022-05-18 21:15:24 +0800

    Add all my files

---
+++ TestForCrudeAPI/TestForCrudeAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TestForCrudeAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TestForCrudeAPI/TestForCrudeAPI/Controllers/GetEmployeeRecordController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestForCrudeAPI.Models;
+
+namespace TestForCrudeAPI.Controllers
+{
+    public class GetEmployeeRecordController : ApiController
+    {
+        Entities db = new Entities();
+        //Insert record
+        public string Post(Employee employee)
+        {
+            db.Employees.Add(employee);
+            db.SaveChanges();
+            return "New employee Added";
+
+        }
+        //Get all Records
+        public IEnumerable<Employee> Get()
+        {
+            return db.Employees.ToList();
+        }
+
+        //Get signle record
+        public Employee Get(int ID)
+        {
+           Employee employee =  db.Employees.Find(ID);
+            return employee;        
+        }
+        //Update record
+        public string Put(int ID, Employee employee)
+        {
+            var employee_ = db.Employees.Find(ID);
+            employee_.FirstName = employee.FirstName;
+            employee_.MiddleName = employee.MiddleName;
+            employee_.LastName = employee.LastName;
+
+            db.Entry(employee_).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return "Employee Record Updated.";
+        }
+
+
+        //Delete record
+        public string Delete(int ID)
+        {
+            Employee employee = db.Employees.Find(ID);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return "Successfully Deleted";
+        }
+    }
+}
